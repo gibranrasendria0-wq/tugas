14096b23c796c9b7e435b4730d2541e16264de22
gibranrasendria0-wq <gibranransendria0@gmail.com>
2026-06-05 21:13:34 +0700

    tugasAja

---
+++ pekan1/jumat.js
@@ -0,0 +1,22 @@
+let nama = "Gibran";
+let umur = 18;
+let statusPelajar = true;
+let hobi = "swimming";
+let tinggiBadan = 164 + "cm";
+let beratBadan = 65 + "kg";
+let makananFavorit = ["pizza", "All fruits", "sate"];
+let kota = "Tangerang";
+let provinsi = "Banten";
+let tahunLahir = "28 april 2008";
+
+console.log('nama: '  + nama);
+console.log('umur:' + umur);
+console.log('status pelajar: ' + statusPelajar);
+console.log('hobi: ' + hobi);
+console.log('tinggi badan: ' + tinggiBadan);
+console.log('berat badan: ' + beratBadan);
+console.log('makanan favorit:' + makananFavorit.join(', '));
+console.log('kota: ' + kota);
+console.log('provinsi: ' + provinsi);
+console.log('tahun lahir: ' + tahunLahir);
+
